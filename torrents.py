e05a34598a85aa8442e9beac0fe7b5f9376773d4
Rehaan Sahay <34886298+sayheyreh@users.noreply.github.com>
2021-07-27 02:59:59 +0530

    replaced some loops with statements

--- torrents.py
+++ torrents.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup;
+from bs4 import BeautifulSoup,SoupStrainer;
 import cfscrape;
 
 domain = 'https://1337x.to/'
@@ -6,16 +6,17 @@ domain = 'https://1337x.to/'
 #input here
 #this takes a search query as input and searches for it, then it returns the first movie link it finds
 def takeInput(search_query):
-    scraper = cfscrape.create_scraper(delay=10)
+    scraper = cfscrape.create_scraper()
     search_query = search_query.strip().replace(' ','+')
     search_url = domain+'search/'+search_query+'/1/'
     print(search_url)
     results = scraper.get(search_url)
+    
+    strainer = SoupStrainer('td', attrs={'class': 'coll-1-name'})
     search_soup = BeautifulSoup(results.content,'html.parser')
     a = search_soup.find('td',attrs={'class': 'coll-1 name'})
-    for links in a.find_all('a'):
-        if links.get('href').startswith('/torrent'):
-            movie_url = domain+links.get('href')[1:]
+    links = a.find_all('a')
+    movie_url = domain+links[1].get('href')[1:]
     return movie_url
 
 url = takeInput(input('Enter query: '))
@@ -33,8 +34,7 @@ for link in soup.find_all('a'):
         download_link = url+link.get('href')[1:]
 
 #hash info
-for hash in soup.find_all('div', attrs={'class': 'infohash-box'}):
-    info_hash = hash.span.text
+info_hash = soup.find('div', attrs={'class': 'infohash-box'}).span.text
 
 #screenshots
 screenshots = []
@@ -42,17 +42,18 @@ for src in soup.find_all('img', attrs={'class': 'img-responsive descrimg'}):
     screenshots.append(src.get('src'))
 
 #cover image
-for img in soup.find_all('img'):
-    if img.get('src').startswith('/img'):
-        cover_img = url+src.get('src')[1:]
+a = soup.find('div',attrs={'class':'torrent-image'})
+cover_img = a.find('img').get('src')
 
 #title, genres and description
 genres=[]
-for a in soup.find_all('div',attrs={'class': 'torrent-detail-info'}):
-    #title
-    title = a.h3.text.strip()
-    #genres
-    for genre in a.find_all('span'):
-        genres.append(genre.text.strip())
-    #description
-    desc = a.p.text.strip()
+a = soup.find('div',attrs={'class': 'torrent-detail-info'})
+#title
+title = a.h3.text.strip()
+#genres
+for genre in a.find_all('span'):
+    genres.append(genre.text.strip())
+#description
+desc = a.p.text.strip()
+
+print(desc)
