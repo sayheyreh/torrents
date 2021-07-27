2072aae083b72e3a3e737339fd8d74e195b3e2c8
Rehaan Sahay <34886298+sayheyreh@users.noreply.github.com>
2021-07-27 12:51:08 +0530

    added error handling for empty search result

--- torrents.py
+++ torrents.py
@@ -11,49 +11,57 @@ def takeInput(search_query):
     search_url = domain+'search/'+search_query+'/1/'
     print(search_url)
     results = scraper.get(search_url)
-    
-    strainer = SoupStrainer('td', attrs={'class': 'coll-1-name'})
     search_soup = BeautifulSoup(results.content,'html.parser')
-    a = search_soup.find('td',attrs={'class': 'coll-1 name'})
-    links = a.find_all('a')
-    movie_url = domain+links[1].get('href')[1:]
-    return movie_url
+    if search_soup.p.text.strip() == 'No results were returned. Please refine your search.':
+        print(search_soup.p.text.strip())
+        return None
+    else:
+        a = search_soup.find('td',attrs={'class': 'coll-1 name'})
+        links = a.find_all('a')
+        movie_url = domain+links[1].get('href')[1:]
+        return movie_url
 
 url = takeInput(input('Enter query: '))
-scraper = cfscrape.create_scraper()
-page = scraper.get(url)
+if url != None:
+    print(url)
+    scraper = cfscrape.create_scraper()
+    page = scraper.get(url)
 
-soup = BeautifulSoup(page.content,'html.parser')
-#magnet and download
-for link in soup.find_all('a'):
-    #magnet
-    if link.get('href').startswith('magnet'):
-        magnet = link.get('href')
-    #download
-    elif link.get('href').startswith('/download'):
-        download_link = url+link.get('href')[1:]
+    soup = BeautifulSoup(page.content,'html.parser')
+    #magnet and download
+    for link in soup.find_all('a'):
+        #magnet
+        if link.get('href').startswith('magnet'):
+            magnet = link.get('href')
+        #download
+        elif link.get('href').startswith('/download'):
+            download_link = url+link.get('href')[1:]
 
-#hash info
-info_hash = soup.find('div', attrs={'class': 'infohash-box'}).span.text
+    #hash info
+    info_hash = soup.find('div', attrs={'class': 'infohash-box'}).span.text
 
-#screenshots
-screenshots = []
-for src in soup.find_all('img', attrs={'class': 'img-responsive descrimg'}):
-    screenshots.append(src.get('src'))
+    #screenshots
+    screenshots = []
+    for src in soup.find_all('img', attrs={'class': 'img-responsive descrimg'}):
+        screenshots.append(src.get('src'))
 
-#cover image
-a = soup.find('div',attrs={'class':'torrent-image'})
-cover_img = a.find('img').get('src')
+    #cover image
+    a = soup.find('div',attrs={'class':'torrent-image'})
+    cover_img = a.find('img').get('src')
 
-#title, genres and description
-genres=[]
-a = soup.find('div',attrs={'class': 'torrent-detail-info'})
-#title
-title = a.h3.text.strip()
-#genres
-for genre in a.find_all('span'):
-    genres.append(genre.text.strip())
-#description
-desc = a.p.text.strip()
+    #title, genres and description
+    genres=[]
+    a = soup.find('div',attrs={'class': 'torrent-detail-info'})
+    #title
+    title = a.h3.text.strip()
+    #genres
+    for genre in a.find_all('span'):
+        genres.append(genre.text.strip())
+    #description
+    desc = a.p.text.strip()
 
-print(desc)
+    #summary
+    summary = soup.find('div',attrs={'id': 'description'}).text
+    text_remove = soup.find('div',attrs={'id':'description'}).strong.text
+    summary = summary.replace(text_remove,'')
+
